e7a8a74ec37f1c59402d2309f0b05dba57b215cb
wisecode007 <y.weiss@outlook.com>
2018-06-21 17:53:16 +0300

    Create ProjectEuler6.cpp

---
+++ ProjectEuler6.cpp
@@ -0,0 +1,23 @@
+//The sum of the squares of the first ten natural numbers is 385
+//The square of the sum of the first ten natural numbers is 3025
+//Hence the difference between the sum of the squares of
+//the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+//Find the difference between the sum of the squares of
+//the first one hundred natural numbers and the square of the sum.
+
+#include <iostream>
+using namespace std;
+void main()
+{
+	int sumOfSquares = 0; //the sum of the numbers in squars
+	int SquareOfSum = 0; //the square of the the sum form 1 to 100 (includ)
+	for (int i = 1; i < 101; i++)
+	{
+		sumOfSquares += i*i;
+		SquareOfSum += i;
+	}
+	SquareOfSum = SquareOfSum * SquareOfSum;
+	int differenceBetweenSums = SquareOfSum - sumOfSquares;
+	cout << differenceBetweenSums << endl;
+}
